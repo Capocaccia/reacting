e973b0073f65da5b22167ace8f2a46132d1d40e8
Carter Capocaccia <carter.capocaccia@gmail.com>
2016-08-31 12:40:37 -0500

    Refactors react to use ES6

--- public/scripts/colorChangeBoxes.js
+++ public/scripts/colorChangeBoxes.js
@@ -5,60 +5,63 @@ var colorList = [
 ];
 var count = 0;
 
-var ColorBoxes = React.createClass({
-    handleClick: function(color){
-        this.setState({
-            color: color
-        })
-    },
-    getInitialState: function(){
-        return {color: this.props.initialColor};
-    },
-    scrollColors:function(colors) {
-        var interval = setInterval(() => {
-            if(count < colors.length){
-                this.handleClick(colors[count].backgroundColor);
-            } else {
-                count = 0;
-                this.handleClick(colors[count].backgroundColor);
-            }
-            count++;
-        }, 1000);
-    },
-    endScrollColors: function(){
-      return;
-    },
-    render: function(){
-        // this
-        var ColorBox = this.props.colorList.map((color, i) => {
-           return (
-               <div onClick={ () => this.handleClick(color.backgroundColor)} key={i} style={{'height' : '100px', 'width': '100px', 'backgroundColor': color.backgroundColor}}>
-               </div>
-           )
-        });
-       return (
-       <div>
-           <h2 style={{color: this.state.color}}>Click a box and I change</h2>
-           <h3>Color Selected: {this.state.color}</h3>
-           {ColorBox}
-           <button onClick={() => this.scrollColors(colorList)}>Cycle through colors</button>
-           <button onClick={() => this.endScrollColors()}>Stop Cycling</button>
-       </div>
-       )
+class ColorBoxes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: this.props.initialColor
+        }
     }
-});
+    handleClick(color){
+    this.setState({
+        color: color
+    })
+    }
+    scrollColors(colors) {
+    this.interval = setInterval(() => {
+        if(count < colors.length){
+            this.handleClick(colors[count].backgroundColor);
+        } else {
+            count = 0;
+            this.handleClick(colors[count].backgroundColor);
+        }
+        count++;
+    }, 1000);
+    }
+    endScrollColors(){
+    clearInterval(this.interval);
+    }
+    render(){
+    // this
+    var ColorBox = this.props.colorList.map((color, i) => {
+        return (
+            <div onClick={ () => this.handleClick(color.backgroundColor)} key={i} style={{'height' : '100px', 'width': '100px', 'backgroundColor': color.backgroundColor}}>
+            </div>
+        )
+    });
+    return (
+        <div>
+            <h2 style={{color: this.state.color}}>Click a box and I change</h2>
+            <h3>Color Selected: {this.state.color}</h3>
+            {ColorBox}
+            <button onClick={() => this.scrollColors(colorList)}>Cycle through colors</button>
+            <button onClick={() => this.endScrollColors()}>Stop Cycling</button>
+        </div>
+    )
+    }
+}
+
+class Text extends React.Component {
+    render(){
+    return (
+        <div>
+            <ColorBoxes initialColor="purple" colorList={this.props.colorList} />
+        </div>
+    )
+    }
+}
 
 
-var Text = React.createClass({
-
-   render: function(){
-       return (
-           <div>
-               <ColorBoxes initialColor="purple" colorList={this.props.colorList} />
-           </div>
-       )
-   }
-});
 
 ReactDOM.render(
   <Text colorList={colorList}  />,
